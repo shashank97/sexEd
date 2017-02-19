da84527d918377e5b109eca6bad01a079294f3cf
taranumesh <Tara@172-16-21-129.DYNAPOOL.NYU.EDU>
2017-02-19 04:42:01 -0500

    ma boi he stops well thanku

--- sexEd/GameScene.swift
+++ sexEd/GameScene.swift
@@ -8,97 +8,74 @@
 import SpriteKit
 import GameplayKit
 
+struct PhysicsCategory
+{
+    static let player : UInt32 = 0x1 << 1
+    static let  border1 : UInt32 = 0x1 << 1
+    static let  border2 : UInt32 = 0x1 << 1
+}
+
 class GameScene: SKScene
 {
     let player = SKSpriteNode(imageNamed: "User")
+    let border1 = SKSpriteNode(imageNamed: "border")
+    let border2 = SKSpriteNode(imageNamed: "border")
     let screenSize: CGRect = UIScreen.main.bounds
+    var touchingScreen = false
+    var yVelocity: CGFloat = -200
     //private var label : SKLabelNode?
     //private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView)
     {
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody?.affectedByGravity = false
         player.position = CGPoint(x: (-screenSize.size.width/2), y: 0)
+        border1.position = CGPoint(x:0, y: screenSize.size.height/2)
+        border1.physicsBody = SKPhysicsBody(rectangleOf: border1.size)
+        border1.physicsBody?.categoryBitMask = PhysicsCategory.border1
+        border1.physicsBody?.categoryBitMask = PhysicsCategory.player
+        border1.physicsBody?.affectedByGravity = false
+        border1.physicsBody?.isDynamic = false
+        border2.position = CGPoint(x:0, y: -screenSize.size.height/2)
+        border2.physicsBody = SKPhysicsBody(rectangleOf: border2.size)
+        border2.physicsBody?.categoryBitMask = PhysicsCategory.border2
+        border2.physicsBody?.categoryBitMask = PhysicsCategory.player
+        border2.physicsBody?.affectedByGravity = false
+        border2.physicsBody?.isDynamic = false
+        addChild(border2)
+        addChild(border1)
         addChild(player)
-        // Get label node from scene and store it for use later
-//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-//        if let label = self.label
-//        {
-//            label.alpha = 0.0
-//            label.run(SKAction.fadeIn(withDuration: 2.0))
-//        }
-//        
-//        // Create shape node to use during mouse interaction
-//        let w = (self.size.width + self.size.height) * 0.05
-//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-//        
-//        if let spinnyNode = self.spinnyNode
-//        {
-//            spinnyNode.lineWidth = 2.5
-//            
-//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-//                                              SKAction.fadeOut(withDuration: 0.5),
-//                                              SKAction.removeFromParent()]))
-//        }
+        print(self.frame.height)
     }
-    
-    func touchDown(atPoint pos : CGPoint)
-    {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode?
-//        {
-//            n.position = pos
-//            n.strokeColor = SKColor.green
-//            self.addChild(n)
-//        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint)
-    {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode?
-//        {
-//            n.position = pos
-//            n.strokeColor = SKColor.blue
-//            self.addChild(n)
-//        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint)
-    {
-//        if let n = self.spinnyNode?.copy() as! SKShapeNode?
-//        {
-//            n.position = pos
-//            n.strokeColor = SKColor.red
-//            self.addChild(n)
-//        }
-    }
-    
+ 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
     {
-//        if let label = self.label
-//        {
-//            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-//        }
-//        
-//        for t in touches
-//        {
-//            self.touchDown(atPoint: t.location(in: self))
-//        }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
-    {
-//        for t in touches
-//        {
-//            self.touchMoved(toPoint: t.location(in: self))
-//        }
+        print(player.position.y)
+        _ = touches.first
+        if (player.position.y <= self.frame.height)
+        {
+            yVelocity = 200
+        }
+        else
+        {
+            yVelocity = 0
+        }
+        touchingScreen = true
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?)
     {
-//        for t in touches
-//        {
-//            self.touchUp(atPoint: t.location(in: self))
-//        }
+        super.touchesEnded(touches, with: event)
+        if (player.position.y > -self.frame.height)
+        {
+            yVelocity = -200
+        }
+        else
+        {
+            yVelocity = 0
+        }
+        touchingScreen = false
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?)
@@ -110,8 +87,9 @@ class GameScene: SKScene
     }
     
     
-    override func update(_ currentTime: TimeInterval)
-    {
-        // Called before each frame is rendered
+    override func update(_ currentTime: CFTimeInterval) {
+        let rate: CGFloat = 0.5; //Controls rate of motion. 1.0 instantaneous, 0.0 none.
+        let relativeVelocity: CGVector = CGVector(dx: 0 , dy:yVelocity-player.physicsBody!.velocity.dy);
+        player.physicsBody?.velocity=CGVector(dx: 0, dy: (player.physicsBody?.velocity.dy)!+relativeVelocity.dy*rate)
+        }
     }
-}
